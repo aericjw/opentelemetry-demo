31eb8ee36be7a350270567abde6d287916860dd6
aericjw <aericwalls26@gmail.com>
2025-12-09 08:25:18 -0700

    full-deployment with DT rum

--- src/frontend/pages/cart/index.tsx
+++ src/frontend/pages/cart/index.tsx
@@ -22,7 +22,8 @@ const Cart: NextPage = () => {
       contextKeys={[...new Set(items.flatMap(({ product }) => product.categories))]}
     >
       <Head>
-        <title>Otel Demo - Cart</title>
+        <title>Otel Demo - Cart</title>\
+        <script type="text/javascript" src="https://js-cdn.dynatracelabs.com/jstag/145e049b9b1/bf08595vkm/b4c9a547b37a0dbc_complete.js" crossorigin="anonymous"></script>
       </Head>
       <Layout>
         <S.Cart>
